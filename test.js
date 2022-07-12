b2e0f048c81bf94a86f1847000765492380ab4a1
yardens01 <yardens01@gmail.com>
2022-07-12 18:34:43 +0300

    test exercise

--- test.js
+++ test.js
@@ -1,10 +1,52 @@
 const puppeteer = require('puppeteer');
-async function run() {
-    const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage()
-    await page.goto('https://chercher.tech/practice/dropdowns');
-    await page.select("select#first", "Apple")
-    await page.waitFor(30000)
+
+//Veriables
+const baseURL = 'http://contractorsinsurancereview.com/ExampleForm/';
+const name = "Yarden";
+const email = "yardens01@gmmail.com";
+const phone= "0545676005";
+const company = "Jones";
+const employees = '51-500';
+
+
+//Puppeteer runner
+(async () => {
+    //Page loaders
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(baseURL);
+
+    //Variable set function
+    await variableSet(page);
+
+    //Screenshot capture
+    await page.screenshot({path: 'mainPageScreenshot.png'})
+
+    //Click button
+    await page.click(".primary.button");
+
+    //Page change check using page title.
+    let title = await page.title();
+    if (title.match("Thank You"))
+        console.log('"Thank You" Page has been reached.');
+
+    
     //await browser.close();
+})();
+
+
+
+async function variableSet(page) {
+    //Added external function and try catch for rubbostness and easy maintenance
+    //For ease of reading didn't make a try-catch for each value, although it's a better practice in case a bug will appear in the future. 
+try {
+    await page.type("#name", name);
+    await page.type("#email", email);
+    await page.type("#phone", phone);
+    await page.type("#company", company);
+    await page.type("#employees", employees);
+} catch (error) {
+    console.log("Variable set error:" + error)
 }
-run()
+    
+}
